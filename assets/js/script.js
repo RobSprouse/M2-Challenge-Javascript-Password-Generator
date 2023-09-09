660afbb2a99d43bbd050fd5a41682a5364fd356b
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2023-09-09 08:04:55 -0400

    Issue with validation prompts fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,33 +14,34 @@ function writePassword() {
 function generatePassword() {
 
   // COMMENT:Defines character sets.
-  let lowercase = "abcdefghijklmnopqrstuvwxyz";
-  let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  let numeric = "0123456789";
-  let special = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+  let lowercaseSet = "abcdefghijklmnopqrstuvwxyz";
+  let uppercaseSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let numericSet = "0123456789";
+  let specialSet = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
   
 
   // COMMENT:Prompts
   let passwordLength = prompt("Choose a password length between 8 and 128 characters.");
     if (passwordLength < 8 || passwordLength > 128) {
-    alert("Please choose a password length between 8 and 128 characters.");
-    generatePassword();
-  }
-      else {
-  let lowercase = confirm("Would you like to include lowercase letters?");
-        alert(lowercase);
-  let uppercase = confirm("Would you like to include uppercase letters?");
-        alert(uppercase);
-  let numeric = confirm("Would you like to include numeric characters?");
-        alert(numeric);
-  let special = confirm("Would you like to include special characters?");
-        alert(special);
-  if (!lowercase + !uppercase + !numeric + !special) {
-      alert("You have to confirm at least one character type!");
-      generatePassword()
+      alert("You have to choose a password length between 8 and 128 characters!");
+      generatePassword();
     }
-  }
-} 
+  let lowercase = confirm("Would you like to include lowercase letters?");
+    alert(lowercase);
+  let uppercase = confirm("Would you like to include uppercase letters?");
+    alert(uppercase);
+  let numeric = confirm("Would you like to include numeric characters?");
+    alert(numeric);
+  let special = confirm("Would you like to include special characters?");
+    alert(special);
+          
+  if (!lowercase && !uppercase && !numeric && !special) {
+            alert("You have to confirm at least one character type!");
+            generatePassword();
+  } 
+    else {console.log("passed")
+    }
+}
 
 
 // TODO:WHEN I click the button to generate a password THEN I am presented with a series of prompts for password criteria
