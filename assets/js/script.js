10b4d38ca75727305fb0b8abbe80ac1163f66428
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2023-09-10 18:45:30 -0400

    modified alert message

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,53 +3,62 @@ let generateBtn = document.querySelector("#generate");
 
 // COMMENT: Declares the function writePassword() which calls the function generatePassword() and assigns the generated password's value to the #password id tag in the html document.
 function writePassword() {
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
-  passwordText.value = password;
+    let password = generatePassword();
+    let passwordText = document.querySelector("#password");
+    passwordText.value = password;
 }
 
 // COMMENT: Declares the function generatePassword()
 function generatePassword() {
+    // COMMENT:Defines password character sets.
+    let lowercaseSet = "abcdefghijklmnopqrstuvwxyz";
+    let uppercaseSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let numericSet = "0123456789";
+    let specialSet = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
-  // COMMENT:Defines password character sets.
-  let lowercaseSet = "abcdefghijklmnopqrstuvwxyz";
-  let uppercaseSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  let numericSet = "0123456789";
-  let specialSet = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-
-  // COMMENT:Prompts for criteria. If the minimum criteria isn't met, the prompts will remain in a "while" loop until the criteria is met.
-  let passwordLength = prompt("While utilizing the numbers on your keyboard, choose a password length between 8 and 128 characters.");
-  while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-    passwordLength = prompt("Your input was not a valid number. Please input a number between 8 and 128 characters for your desired password length.");
-  }
-  let lowercase = confirm("Would you like for your password to include lowercase letters?");
-    alert(lowercase);
-  let uppercase = confirm("Would you like for your password to include uppercase letters?");
+    // COMMENT:Prompts for criteria. If the minimum criteria isn't met, the prompts will remain in a "while" loop until the criteria is met.
+    let passwordLength = prompt(
+        "While utilizing the numbers on your keyboard, choose a password length between 8 and 128 characters."
+    );
+    while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+        passwordLength = prompt(
+            "Your input was not a valid number. Please input a number between 8 and 128 characters for your desired password length."
+        );
+    }
+    let lowercase = confirm("Would you like for your password to include lowercase letters?");
+    let lowercaseMessage
+    if (!lowercase) {
+        lowercaseMessage = "You have chosen not to include lowercase letters."
+    } else {
+        lowercaseMessage = "You have chosen to include lowercase letters."
+    }
+    alert(lowercaseMessage);
+    let uppercase = confirm("Would you like for your password to include uppercase letters?");
     alert(uppercase);
-  let numeric = confirm("Would you like for your password to include numeric characters?");
+    let numeric = confirm("Would you like for your password to include numeric characters?");
     alert(numeric);
-  let special = confirm("Would you like for your password to include special characters?");
+    let special = confirm("Would you like for your password to include special characters?");
     alert(special);
-  while (!lowercase && !uppercase && !numeric && !special) {
-    alert("At least one character type must be selected. Please select at least one character type.");
-    lowercase = confirm("Would you like for your password to include lowercase letters?");
-    uppercase = confirm("Would you like for your password to include uppercase letters?");
-    numeric = confirm("Would you like for your password to include numeric characters?");
-    special = confirm("Would you like for your password to include special characters?");
-  } 
-  // COMMENT: Defines a random characterSet based on the criteria chsoen by the user and generates a random password, then stores it into a variable called password.
-   let characterSet = "";
-     if (lowercase) characterSet += lowercaseSet;
-     if (uppercase) characterSet += uppercaseSet;
-     if (numeric) characterSet += numericSet; 
-     if (special) characterSet += specialSet;
-     let password = "";
-       for (let i = 0; i < passwordLength; i++) {
-         password += characterSet[Math.floor(Math.random() * characterSet.length)];
-       }
-   return password;
- }
- 
+    while (!lowercase && !uppercase && !numeric && !special) {
+        alert("At least one character type must be selected. Please select at least one character type.");
+        lowercase = confirm("Would you like for your password to include lowercase letters?");
+        uppercase = confirm("Would you like for your password to include uppercase letters?");
+        numeric = confirm("Would you like for your password to include numeric characters?");
+        special = confirm("Would you like for your password to include special characters?");
+    }
+    // COMMENT: Defines a random characterSet based on the criteria chsoen by the user and generates a random password, then stores it into a variable called password.
+    let characterSet = "";
+    if (lowercase) characterSet += lowercaseSet;
+    if (uppercase) characterSet += uppercaseSet;
+    if (numeric) characterSet += numericSet;
+    if (special) characterSet += specialSet;
+    let password = "";
+    for (let i = 0; i < passwordLength; i++) {
+        password += characterSet[Math.floor(Math.random() * characterSet.length)];
+    }
+    return password;
+}
+
 // COMMENT:Add an event listener to the #generate button in the html document which starts the function writePassword() when the button is clicked.
 generateBtn.addEventListener("click", writePassword);
 
