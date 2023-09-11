f609708810e1d3f57e348c02931bfa7e0ba743d6
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2023-09-11 11:28:19 -0400

    Readme in progress

--- README.md
+++ README.md
@@ -1,2 +1,95 @@
 # M2-Challenge-Javascript-Password-Generator
-Password-Generator 
+
+## Description
+
+This Repository was created to complete the University of Richmond's Coding Bootcamp, Module 3 Challenge. The challenge required me to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected.
+
+The following User Story and Acceptance Criteria was used to achieve the primary objective.
+
+> ## User Story
+>
+> ```
+> AS AN employee with access to sensitive data
+> I WANT to randomly generate a password that meets certain criteria
+> SO THAT I can create a strong password that provides greater security
+> ```
+>
+> ## Acceptance Criteria
+>
+> ```
+> GIVEN I need a new, secure password
+> WHEN I click the button to generate a password
+> THEN I am presented with a series of prompts for password criteria
+> WHEN prompted for password criteria
+> THEN I select which criteria to include in the password
+> WHEN prompted for the length of the password
+> THEN I choose a length of at least 8 characters and no more than 128 characters
+> WHEN asked for character types to include in the password
+> THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+> WHEN I answer each prompt
+> THEN my input should be validated and at least one character type should be selected
+> WHEN all prompts are answered
+> THEN a password is generated that matches the selected criteria
+> WHEN the password is generated
+> THEN the password is either displayed in an alert or written to the page
+> ```
+
+In addition to meeting the above criteria, I wrote the code to prevent "undefined" errors from appearing when the user's input is invalid.
+
+## Usage
+
+This application was deployed to the following URL.
+
+>https://robsprouse.github.io/M3-Challenge-Javascript-Password-Generator/
+
+The following image shows the web application's appearance. 
+
+><img src="./assets/screenshots/JavascriptPasswordGenerator.png" margin="500px">
+<br>
+The following image shows the modified js code that generates passwords based on the required criteria. 
+<br>
+
+
+><img src="./assets/screenshots/js-code.png">
+
+## License
+
+MIT License
+
+Copyright (c) 2023 Robert Sprouse
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+## Features
+
+If your project has a lot of features, list them here.
+
+## How to Contribute
+
+If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+## Tests
+
+Go the extra mile and write tests for your application. Then provide examples on how to run them here.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
-// COMMENT: Assigns the generateBtn element to the button with id "generate" in the html document
+// COMMENT: js file for password generator
+
+// COMMENT: Assigns the generateBtn element to the button with the id "generate" in the html document
 let generateBtn = document.querySelector("#generate");
 
 // COMMENT: Declares the function writePassword() which calls the function generatePassword() and assigns the generated password's value to the #password id tag in the html document.
@@ -18,56 +20,53 @@ function generatePassword() {
 
     // COMMENT:Prompts for criteria. If the minimum criteria isn't met, the prompts will remain in a "while" loop until the criteria is met.
     let passwordLength = prompt(
-        "While utilizing the numbers on your keyboard, choose a password length between 8 and 128 characters."
+        "While utilizing the numbers on your keyboard, choose a password length of at least 8 characters and no more than 128 characters."
     );
     while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
         passwordLength = prompt(
-            "Your input was not a valid number. Please input a number between 8 and 128 characters for your desired password length."
+            "Your input was not a valid number or within the proper range. Please choose a password length of at least 8 and no more than 128 characters."
         );
     }
     let lowercase = confirm("Would you like for your password to include lowercase letters?");
     let uppercase = confirm("Would you like for your password to include uppercase letters?");
-    alert(uppercase);
     let numeric = confirm("Would you like for your password to include numeric characters?");
-    alert(numeric);
     let special = confirm("Would you like for your password to include special characters?");
-    alert(special);
     while (!lowercase && !uppercase && !numeric && !special) {
         alert("At least one character type must be selected. Please select at least one character type.");
         lowercase = confirm("Would you like for your password to include lowercase letters?");
-        let lowercaseMessage
+        let lowercaseMessage;
         if (!lowercase) {
-            lowercaseMessage = "You have chosen not to include lowercase letters."
+            lowercaseMessage = "You have chosen not to include lowercase letters.";
         } else {
-            lowercaseMessage = "You have chosen to include lowercase letters."
+            lowercaseMessage = "You have chosen to include lowercase letters.";
         }
         alert(lowercaseMessage);
         uppercase = confirm("Would you like for your password to include uppercase letters?");
-        let uppercaseMessage
+        let uppercaseMessage;
         if (!uppercase) {
-            uppercaseMessage = "You have chosen not to include uppercase letters."
+            uppercaseMessage = "You have chosen not to include uppercase letters.";
         } else {
-            uppercaseMessage ="You have chosen to include uppercase letters."
+            uppercaseMessage = "You have chosen to include uppercase letters.";
         }
         alert(uppercaseMessage);
         numeric = confirm("Would you like for your password to include numeric characters?");
         let numericMessage;
         if (!numeric) {
-            numericMessage = "You have chosen not to include numeric characters."
+            numericMessage = "You have chosen not to include numeric characters.";
         } else {
-            numericMessage = "You have chosen to include numeric characters."
+            numericMessage = "You have chosen to include numeric characters.";
         }
-        alert (numericMessage);
+        alert(numericMessage);
         special = confirm("Would you like for your password to include special characters?");
         let specialMessage;
         if (!special) {
-            specialMessage = "You have chosen not to include special characters."
+            specialMessage = "You have chosen not to include special characters.";
         } else {
-            specialMessage = "You have chosen to include special characters."
+            specialMessage = "You have chosen to include special characters.";
         }
-        alert (specialMessage);
+        alert(specialMessage);
     }
-    // COMMENT: Defines a random characterSet based on the criteria chsoen by the user and generates a random password, then stores it into a variable called password.
+    // COMMENT: Defines a random characterSet based on the criteria chosen by the user and generates a random password, then stores it into a variable called password.
     let characterSet = "";
     if (lowercase) characterSet += lowercaseSet;
     if (uppercase) characterSet += uppercaseSet;
@@ -79,20 +78,5 @@ function generatePassword() {
     }
     return password;
 }
-
 // COMMENT:Add an event listener to the #generate button in the html document which starts the function writePassword() when the button is clicked.
 generateBtn.addEventListener("click", writePassword);
-
-// TODO:WHEN I click the button to generate a password THEN I am presented with a series of prompts for password criteria
-
-// TODO: WHEN prompted for password criteria THEN I select which criteria to include in the password
-
-// TODO: WHEN prompted for the length of the password THEN I choose a length of at least 8 characters and no more than 128 characters
-
-// TODO: WHEN asked for character types to include in the password THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-
-// TODO: WHEN I answer each prompt THEN my input should be validated and at least one character type should be selected
-
-// TODO: WHEN all prompts are answered THEN a password is generated that matches the selected criteria
-
-// TODO: WHEN the password is generatedTHEN the password is either displayed in an alert or written to the page
